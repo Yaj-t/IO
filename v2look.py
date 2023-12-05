f9b1d5fa6dd0f4120493d01f95c921a9e3582d4b
T-jay A. Abunales <tjabunales@gmail.com>
2023-12-06 01:07:08 +0800

    v2

--- v2look.py
+++ v2look.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import random
 def LOOK(arr, head, direction):
     """
     Function to perform the LOOK algorithm
@@ -48,9 +50,26 @@ def LOOK(arr, head, direction):
     return seek_sequence
 
 # Example usage
-arr = [176, 79, 34, 60, 92, 11, 41, 114]
+arr = [random.randint(0, 199) for _ in range(20)]
+# arr = [176, 79, 34, 60, 92, 11, 41, 114]
 head = 176
 direction = "left"
 
+print("array: {arr} head: {head} direction: {direction}")
+
 sequence = LOOK(arr, head, direction)
 print("The sequence of movements is:", sequence)
+
+# Plotting the seek sequence with labels
+plt.figure(figsize=(10, 5))
+plt.plot(range(len(sequence)), sequence, marker='o', linestyle='-')
+plt.xlabel("Index")
+plt.ylabel("Size")
+plt.title("LOOK Algorithm Seek Sequence")
+
+# Adding labels to each data point
+for i, size in enumerate(sequence):
+    plt.annotate(f"{size}", (i, size), textcoords="offset points", xytext=(0, 10), ha='center')
+
+plt.grid(True)
+plt.show()
